8a5a2e47b1b22734faceb4f9d082488fa9fa591d
student <nikitamendukov@gmail.com>
2023-05-11 20:24:20 +0300

    Person example

--- Успадкування_11.05.py
+++ Успадкування_11.05.py
@@ -12,12 +12,31 @@ class Dog(Animal):
     def bark(self):
         print(self.name, 'гавкає.')
     def __str__(self):
-        return hotdog.name, hotdog.age, hotdog.breed
+        return self.name
+
 hotdog = Dog('Sharick', 3, 'Mops')
 
 print(hotdog.name, hotdog.age, hotdog.breed)
-
+print(hotdog)
 hotdog.bark()
 hotdog.eat()
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f'Name: {self.name}, Age: {self.age}'
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+    def __str__(self):
+        return super().__str__() + f' , student_id: {self.student_id}'
+
+student1 = Student('Oleg', 46, '32er34')
+
+with open('info_studebt.txt', 'w') as file:
+    file.write(str(student1))
 
 
