b89a60aa889bd888c9f844c0a999e6e03794bd3b
student <nikitamendukov@gmail.com>
2023-05-16 19:44:55 +0300

    try/exept 1-3

--- class_16.05.py
+++ class_16.05.py
@@ -1,3 +1,4 @@
+#1
 try:
     print('Start:')
     print(error)  #після цього рядка программа не продовжується
@@ -6,3 +7,24 @@ except:
     print('We have a problem')
 
 
+#2
+def checker(var_1):
+    if type(var_1) != str:
+        raise TypeError(f'Неможливо працювати з типом {type(var_1)}, потібно str')
+    else:
+        print(var_1)
+first = 6
+checker(first)
+
+#3
+class BuildingError(Exception):
+    def __str__(self):
+        return 'Неможливо збудувати будівлю, матеріальів замало '
+def check_materail(amount_of_material, limit_value):
+    if amount_of_material > limit_value:
+        return 'Все добре'
+    else:
+        raise BuildingError()
+
+materials = 123
+print(check_materail(materials, 300))
